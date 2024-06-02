a118f135659823a4bcec5877ed686a088b9ac01c
Tyler Johnston <tylergarrettjohnston@gmail.com>
2024-06-02 15:55:28 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const fs = require("fs");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const upload = require("./middleware/upload");
 
-
+app.post("/api/fileanalyse", upload.single("upfile"), async (req, res) => {
+  try {
+    const { filename, originalname, mimetype, size } = req.file;
+    const path = __dirname + "/uploads/" + filename;
+    fs.unlinkSync(path);
+    console.log(`Removed file at path: ${path}`);
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size: size,
+    });
+  } catch (err) {
+    console.error(err);
+    res.json({ error });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
